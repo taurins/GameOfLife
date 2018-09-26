2e710d527ae7124dc6d18ff04a4eeca1c8532efc
Raivis Tauriņš <taurins94@gmail.com>
2018-09-26 11:19:11 +0300

    Fixed the bug, can't understand why whas there a bug

--- GameOfLife2/DataManipulation/FieldUpdate.cs
+++ GameOfLife2/DataManipulation/FieldUpdate.cs
@@ -16,13 +16,10 @@ namespace GameOfLife2.DataManipulation
             Neighbors = new Neighbors(Field.Cells);
         }
 
-        //need to fux bug
         public void UpdateField()
         {
             Field.Iteration += 1;
             Field TempField = new Field(new TwoIntegerVariable { X = Field.Cells.GetLength(0), Y = Field.Cells.GetLength(1) });
-            //CellPopulator Populator = new CellPopulator(TempField);
-            //Populator.RandomPopulation();
             for (int x = 0; x < Field.Cells.GetLength(0); x++)
             {
                 for (int y = 0; y < Field.Cells.GetLength(1); y++)
@@ -30,35 +27,15 @@ namespace GameOfLife2.DataManipulation
                     Cell c = new Cell();
                     TempField.Cells[x, y] = new Cell();
                     CellUpdate Update = new CellUpdate(c);
+                    Update.ChangeStatus(Field.Cells[x, y].IsAlive);
 
+                    Update.ChangeStatus(new Neighbors(Field.Cells).GetNeighborCount(x, y));
+                    //Update.ChangeStatus(Neighbors().GetNeighborCount(x, y)); bug is here
+                    Update = new CellUpdate(TempField.Cells[x, y]);
                     Update.ChangeStatus(c.IsAlive);
-                    int count = Neighbors.GetNeighborCount(x, y);
-                    Update.ChangeStatus(count);
-
-                    TempField.Cells[x, y] = c;
-
-                    //CellUpdate Update2 = new CellUpdate(TempField.Cells[x, y]);
-                    //Update2.ChangeStatus(c.IsAlive);
-
-                    //TempField.Cells[x, y] = new Cell();
-
-                    //CellUpdate Update = new CellUpdate(TempField.Cells[x, y]);
-
-                    //Update.ChangeStatus(Field.Cells[x, y].IsAlive);
-                    //int count = Neighbors.GetNeighborCount(x, y);
-                    //Update.ChangeStatus(count);
-
-                    //Update = new CellUpdate(TempField.Cells[x, y]);
-                    //TempField.Cells[x, y].IsAlive = c.IsAlive;
-                    // Update.ChangeStatus(c.IsAlive);
-                    //Update = new CellUpdate(TempField.Cells[x, y]);
-
-                    //Update.ChangeStatus(Neighbors.GetNeighborCount(x, y));
-                    //TempField.Cells[x, y] = c;
                 }
             }
             int AliveCells = new AliveCells(Field.Cells).GetAliveCellsCount();
-            //Field.Cells = new Cell[Field.Cells.GetLength(0), Field.Cells.GetLength(1)];
             Field.Cells = TempField.Cells;
         }
     }
